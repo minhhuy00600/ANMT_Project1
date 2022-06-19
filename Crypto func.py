34a7850525ab9fe9ca7369db6d02d037b28c7e38
minhhuy00600 <minhhuy00600@gmail.com>
2022-06-20 06:39:48 +0700

    Xong AES encrypt
    AES decrypt thì cần sửa theo UI . Tại t chưa biết là AES để mã hóa private key của RSA rồi decrypt lại như nào nên từ từ mình bàn sau . Nói chung là phụ thuộc vào thằng làm UI như nào á ( t nghĩ d )

--- Crypto func.py
+++ Crypto func.py
@@ -1,6 +1,21 @@
 import hashlib
+
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives.asymmetric import rsa
+
+from Crypto.Cipher import AES
+
+
+# key = b'Sixteen byte key'  # Passphrase use to create key
+# cipher = AES.new(key, AES.MODE_EAX)
+# nonce = cipher.nonce
+# ciphertext, tag = cipher.encrypt_and_digest(b'abcddd')
+# print(cipher, '\n', key, '\n', nonce, '\n')
+# print('ciphertext :', ciphertext)
+#
+# cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+# plaintext = cipher.decrypt(ciphertext)
+# print('plaintext :', plaintext)
 
 
 def hash_256(string):
@@ -8,7 +23,27 @@ def hash_256(string):
     return hashed_string
 
 
-# def aes_enc(passphase, Kprivate):
+def aes_enc(passphrase, Kprivate):
+    key = str.encode(passphrase)  # Passphrase use to create key
+
+    if len(key) < 16:
+        padding = '%0'  # Padding if key < 16 length
+        length = 16
+
+        key = key.rjust(length, padding)  # Padding key
+
+    cipher = AES.new(key, AES.MODE_EAX)
+    nonce = cipher.nonce
+
+    # Encrypt start . Kprivate must be bytes type
+    Kprivate_enc_ed, tag = cipher.encrypt_and_digest(Kprivate)
+    return Kprivate_enc_ed
+
+
+def aes_dec(ciphertext, key, nonce):  # Decryption , ciphertext must be bytes type
+    cipher = AES.new(key, AES.MODE_EAX, nonce=nonce)
+    plaintext = cipher.decrypt(ciphertext)
+    #  Can sua theo UI
 
 
 def rsa_keygen():
@@ -37,3 +72,13 @@ def rsa_keygen():
 
     pri_w.close()
     pub_w.close()
+
+
+# RSA Key loading
+# from cryptography.hazmat.primitives import serialization
+#
+# with open("path/to/key.pem", "rb") as key_file:
+#     private_key = serialization.load_pem_private_key(
+#         key_file.read(),
+#         password=None,
+#     )
